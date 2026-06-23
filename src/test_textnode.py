97f55eed6264f9eaaad06bc237b3370b57b4ce42
Ethem-Sahbaz <ethemsahbaz@outlook.com>
2026-06-23 12:26:07 +0200

    wrote tests for textnode class

---
+++ src/test_textnode.py
@@ -0,0 +1,22 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_url_is_none_by_default(self):
+        node = TextNode("Url is None text node", TextType.CODE)
+        self.assertEqual(node.url, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
