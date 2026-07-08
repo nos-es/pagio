a64512124fda661ddb7e63a6cf92e9f2d0f812ea
Ethem-Sahbaz <ethemsahbaz@outlook.com>
2026-07-08 11:40:46 +0200

    deploy to the internet

--- src/pagio/main.py
+++ src/pagio/main.py
@@ -1,14 +1,15 @@
 from pagio.services.copystatics import copy_source_to_destination
 from pagio.services.generatepages import generate_pages_recursive
+import sys
 
 
 def main() -> None:
-    copy_source_to_destination("./static/", "./public/")
-    generate_pages_recursive("./content/", "./template.html", "./public")
-    # generate_page("./content/index.md", "./template.html",
-    #               "./public/index.html")
-
-    pass
+    base_path = "/"
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+    print(base_path)
+    copy_source_to_destination("./static/", "./docs/")
+    generate_pages_recursive("./content/", "./template.html", "./docs/", base_path)
 
 
 if __name__ == "__main__":
--- src/pagio/services/generatepages.py
+++ src/pagio/services/generatepages.py
@@ -24,7 +24,7 @@ def extract_title(markdown: str) -> str:
     return title
 
 
-def generate_page(from_path, template_path, dest_path) -> None:
+def generate_page(from_path, template_path, dest_path, base_path) -> None:
     print(f"Generating page from {from_path} to {
           dest_path} using {template_path}")
 
@@ -41,6 +41,8 @@ def generate_page(from_path, template_path, dest_path) -> None:
 
     html_page = template.replace("{{ Title }}", title)
     html_page = html_page.replace("{{ Content }}", content)
+    html_page = html_page.replace('href="/', f'href="{base_path}')
+    html_page = html_page.replace('src="/', f'src="{base_path}')
 
     dest_dir_path = os.path.dirname(dest_path)
 
@@ -51,7 +53,7 @@ def generate_page(from_path, template_path, dest_path) -> None:
         html_file.write(html_page)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path) -> None:
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path) -> None:
 
     for name in os.listdir(dir_path_content):
         source_item = os.path.join(dir_path_content, name)
@@ -61,8 +63,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path) -> 
 
         if os.path.isfile(source_item):
             target_item = target_item.replace(".md", ".html")
-            generate_page(source_item, template_path, target_item)
+            generate_page(source_item, template_path, target_item, base_path)
         elif os.path.isdir(source_item):
-            generate_pages_recursive(source_item, template_path, target_item)
+            generate_pages_recursive(
+                source_item, template_path, target_item, base_path)
         else:
             print(f"Skipping unsupported file type: {source_item}")
